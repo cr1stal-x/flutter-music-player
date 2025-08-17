4a277f68e229a76f025436433460988a200df9e0
sara <saray.mohammadi84@gmail.com>
2025-08-17 13:40:22 +0330

    edit Comment class instead of sql

--- lib/src/models/Comment.java
+++ lib/src/models/Comment.java
@@ -1,39 +1,72 @@
+import java.util.Date;
+
 public class Comment {
-    private String text;
+    private int commentId;
+    private int songId;
+    private int userId;
+    private String content;
+    private Date createdAt;
     private int likes;
     private int dislikes;
 
-    public Comment(String text, int likes, int dislikes) {
-        this.text = text;
-        this.likes = likes;
-        this.dislikes = dislikes;
+    public Comment(int commentId, int songId, int userId, String content, Date createdAt) {
+        this.commentId = commentId;
+        this.songId = songId;
+        this.userId = userId;
+        this.content = content;
+        this.createdAt = createdAt;
+        this.likes = 0;
+        this.dislikes = 0;
     }
 
-    public String getText() {
-        return text;
+    public int getCommentId() {
+        return commentId;
     }
+
+    public int getSongId() {
+        return songId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
     public int getLikes() {
         return likes;
     }
+
     public int getDislikes() {
         return dislikes;
     }
-    public void setText(String text) {
-        this.text = text;
+
+    public void setContent(String content) {
+        this.content = content;
     }
-    public void setLikes(int likes) {
-        this.likes = likes;
+
+    public void like() {
+        likes++;
     }
-    public void setDislikes(int dislikes) {
-        this.dislikes = dislikes;
+
+    public void dislike() {
+        dislikes++;
     }
 
     @Override
     public String toString() {
-        return "comment{" +
-                "idea='" + text + '\'' +
-                ", likes='" + likes + '\'' +
-                ", dislikes='" + dislikes + '\'' +
-                '}';
+        return "Comment ID: " + commentId +
+                ", Song ID: " + songId +
+                ", User ID: " + userId +
+                ", Content: " + content +
+                ", Created At: " + createdAt +
+                ", Likes: " + likes +
+                ", Dislikes: " + dislikes;
     }
 }
